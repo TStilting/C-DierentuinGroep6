92e737f13609004478422bfc7c9cf142f3fe09c9
Troy <troy.stilting@st-2.com>
2024-12-06 11:53:49 +0100

    Animal en Enclosures aan domeinmodel toegevoegd

--- DierentuinGroep6/Models/Animal.cs
+++ DierentuinGroep6/Models/Animal.cs
@@ -1,12 +1,50 @@
-﻿namespace DierentuinGroep6.Models
+﻿using System.Drawing;
+
+namespace DierentuinGroep6.Models
 {
     public class Animal
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Species { get; set; }
+        private string Category { get; set; }
+        private Size Size { get; set; }
+        private DietaryClass DietaryClass { get; set; }
+        private ActivityPattern ActivityPattern { get; set; }
+        private List<string> Prey { get; set; }
+        private string Enclosure { get; set; }
+        private double SpaceRequirement { get; set; }
+        private SecurityLevel SecurityRequirement { get; set; } 
+
 
         public Animal() { 
         
         }
+    }
+
+    public enum Size
+    {
+        Microscopic,
+        VerySmall,
+        Small,
+        Medium,
+        Large,
+        VeryLarge
+    }
+
+    public enum DietaryClass
+    {
+        Carnivore,
+        Herbivore,
+        Omnivore,
+        Insectivore,
+        Piscivore
+    }
+
+    public enum ActivityPattern
+    {
+        Diurnal,
+        Nocturnal,
+        Cathemeral
     }
 }
--- DierentuinGroep6/Models/Enclosure.cs
+++ DierentuinGroep6/Models/Enclosure.cs
@@ -2,11 +2,43 @@
 {
     public class Enclosure
     {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private List<Animal> Animals { get; set; } = new();
+        private Climate Climate { get; set; }
+        private HabitatType HabitatType { get; set; }
+        private SecurityLevel SecurityLevel { get; set; }
+        private double Size { get; set; } // in square meters
 
-
-        public Enclosure()
+        public Enclosure(int id, string name, Climate climate, HabitatType habitatType, SecurityLevel securityLevel, double size)
         {
-
+            Id = id;
+            Name = name;
+            Climate = climate;
+            HabitatType = habitatType;
+            SecurityLevel = securityLevel;
+            Size = size;
         }
+    }
+
+    public enum Climate
+    {
+        Tropical,
+        Temperate,
+        Arctic
+    }
+    [Flags]
+    public enum HabitatType
+    {
+        Forest = 1,
+        Aquatic = 2,
+        Desert = 4,
+        Grassland = 8
+    }
+    public enum SecurityLevel
+    {
+        Low,
+        Medium,
+        High
     }
 }
